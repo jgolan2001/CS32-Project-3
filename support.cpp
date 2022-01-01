b9c69d602f760389f76d77f717278f840ec36a2d
jgolan2001 <96957868+jgolan2001@users.noreply.github.com>
2022-01-01 13:07:17 -0800

    Add files via upload

---
+++ support.cpp
@@ -0,0 +1,8 @@
+//
+//  support.cpp
+//  Project 3
+//
+//  Created by Jordan Golan on 6/3/21.
+//
+
+#include "support.h"
---
+++ support.h
@@ -0,0 +1,13 @@
+//
+//  support.hpp
+//  Project 3
+//
+//  Created by Jordan Golan on 6/3/21.
+//
+
+#ifndef support_h
+#define support_h
+
+#include <stdio.h>
+
+#endif /* support_h */
